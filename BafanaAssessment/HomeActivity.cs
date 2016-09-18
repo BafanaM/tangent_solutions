da6bf537b695269dff221bd1d0e0375de47c4809
Bafana Mankahla <mankahla.bafana@gmail.com>
2016-09-18 21:09:45 +0200

    Added actionbar and drawer layout.

--- BafanaAssessment/HomeActivity.cs
+++ BafanaAssessment/HomeActivity.cs
@@ -9,16 +9,25 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
 
 namespace BafanaAssessment
 {
-    [Activity(Label = "Home Activity")]
-    public class HomeActivity : Activity
+    [Activity(Label = "Home Activity",Theme = "@style/navigation_drawer_theme")]
+    public class HomeActivity : AppCompatActivity
     {
+
+        private SupportToolbar mToolbar;
+
+  
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Home_Activity);
+
+            mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(mToolbar);
             // Create your application here
 
 
